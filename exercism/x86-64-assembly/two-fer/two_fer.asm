7f9d56247c71125bdc903f4b8aa7dfe6586b3743
Felipe Rodrigues <felipe@crafters.cc>
2019-11-27 00:15:33 -0300

    Using macros on nasm two_fer exercise

--- exercism/x86-64-assembly/two-fer/two_fer.asm
+++ exercism/x86-64-assembly/two-fer/two_fer.asm
@@ -1,6 +1,19 @@
 	default	rel
 	section	.text
 	global	two_fer
+
+%macro copy_str 1
+%%loop:
+	mov	cl, [%1] 		; Get 1 byte from the message to cl
+	cmp	cl, 0 			; Check if the byte in cl is NULL
+	je	%%end 			; Start copying the name if cl is NULL
+	mov 	[rsi], cl 		; Insert in *buffer
+	inc 	rsi 			; Move rsi to the next slot
+	inc 	%1 			; Move message to the next byte
+	jmp 	%%loop 			; Loop back to copy the next byte
+%%end: 					; end label to finish the macro
+%endmacro
+
 two_fer:
 	lea	r8, [msg_prefix]	; Get the address of the message prefix
 	lea	r10, [msg_suffix]	; Get the address of the message suffix
@@ -9,38 +22,20 @@ two_fer:
 	jne	copy_prefix		; Start copying the prefix message if a name is given
 
 set_default_name:
-	lea	r9, [you]		; Get the default name address 
+	lea	r9, [you]		; Get the default name address
 
 copy_prefix:
-	mov	cl, [r8]		; Get 1 byte from the message to cl
-	cmp	cl, 0			; Check if the byte in cl is NULL
-	je	copy_name		; Start copying the name if cl is NULL
-	mov	[rsi], cl		; Insert in *buffer
-	inc	rsi			; Move rsi to the next slot
-	inc	r8			; Move r8 to the next byte 
-	jmp	copy_prefix		; Loop back to copy the next byte in prefix
+	copy_str r8
 
 copy_name:
-	mov	cl, [r9]		; Get 1 byte from the name
-	cmp 	cl, 0			; Check if the byte in cl is NULL
-	je 	copy_suffix		; Start copying the suffix if cl is NULL
-	mov	[rsi], cl		; Insert in *buffer
-	inc	rsi			; Move rsi to the next slot
-	inc	r9			; Move r9 to the next byte
-	jmp	copy_name		; Loop back to copy the next byte in name
+	copy_str r9
 
 copy_suffix:
-	mov	cl, [r10]		; Get 1 byte from the name
-	mov	[rsi], cl		; Insert in * buffer
-	cmp	cl, 0			; Check if the byte in cl is NULL
-	je	return			; Jump to return if cl is NULL (end of string)
-	inc	rsi			; Move rsi to the next slot
-	inc	r10			; Move r10 to the next byte
-	jmp	copy_suffix		; Loop back to copy the next byte in suffix
+	copy_str r10
+	mov cl, [r10]
+	mov [rsi], cl
+	ret 				; End the program
 
-return:
-	ret				; End the program
-	
 	section .data
 msg_prefix:	db "One for ", 0
 msg_suffix:	db ", one for me.", 0
