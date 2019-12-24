7615440b2c7fc9eda1c33a7be352eedacfa32774
Felipe Rodrigues <felipe@crafters.cc>
2019-12-24 15:13:39 -0300

    Fixing R Leap Year

--- exercism/r/leap/leap.R
+++ exercism/r/leap/leap.R
@@ -1,8 +1,3 @@
 leap <- function(year) {
-  if(year %% 4 != 0)
-    return(FALSE)
-  else if (year %% 100 == 0 && year %% 400 != 0)
-    return(FALSE)
-
-  TRUE
+	year %% 4 == 0 && year %% 100 != 0 || year %% 400 == 0
 }
