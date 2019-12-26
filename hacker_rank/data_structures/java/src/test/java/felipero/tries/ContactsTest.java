219f6e473f237dbdcf379d6a551e2ef182530067
Felipe Rodrigues <felipe@crafters.cc>
2019-12-26 19:55:36 -0300

    Testing with large number of operations from file

--- hacker_rank/data_structures/java/src/test/java/felipero/tries/ContactsTest.java
+++ hacker_rank/data_structures/java/src/test/java/felipero/tries/ContactsTest.java
@@ -1,12 +1,24 @@
 package felipero.tries;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 class ContactsTest {
   @Test
   void testContacts() {
-    StringBuffer input = new StringBuffer("5\n");
+    StringBuilder input = new StringBuilder("5\n");
     input.append("add felipe\n");
     input.append("add falcao\n");
     input.append("add felipero\n");
@@ -18,7 +30,7 @@ class ContactsTest {
 
   @Test
   void testContacts_with_lots_of_s() {
-    StringBuffer input = new StringBuffer("11\n");
+    StringBuilder input = new StringBuilder("11\n");
     input.append("add s\n");
     input.append("add ss\n");
     input.append("add sss\n");
@@ -32,5 +44,25 @@ class ContactsTest {
     input.append("find ssssss");
 
     assertEquals("5\n4\n3\n2\n1\n0", Contacts.execute(input.toString()));
+  }
+
+  @Test
+  @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+  void testContacts_from_input_file() throws IOException {
+    String input = readFile("src/test/resources/contacts_input02.txt");
+    String output = readFile("src/test/resources/contacts_output02.txt");
+
+    assertEquals(output, Contacts.execute(input) + "\n");
+  }
+
+  private String readFile(String path) throws IOException {
+    StringBuilder content = new StringBuilder();
+
+    try (Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
+      stream.forEach(s -> content.append(s).append("\n"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return content.toString();
   }
 }
