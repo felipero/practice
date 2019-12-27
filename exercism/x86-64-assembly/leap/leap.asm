8abc900b14076434f59e12102d8c1b2d6bf9d494
Felipe Rodrigues <felipe@crafters.cc>
2019-12-27 16:17:21 -0300

    Zero the RDX register to prevent garbage.

--- exercism/x86-64-assembly/leap/leap.asm
+++ exercism/x86-64-assembly/leap/leap.asm
@@ -2,25 +2,23 @@ section .text
 global leap_year
 
 %macro divide_by 1
+	xor	rdx, rdx
 	mov	rax, rdi
-	mov	rbx, %1
-	div	rbx
+	mov	r8, %1
+	div	r8
 	cmp	rdx, 0
 %endmacro
 
 leap_year:
 
-div_4:
 	divide_by 4
-	jne	return_false  ; Not divisible by 4
+	jne	return_false	; Not divisible by 4
 
-div_100:
 	divide_by 100
-	jne	return_true  ; Not divisible by 100
+	jne	return_true	; Not divisible by 100
 
-div_400:
 	divide_by 400
-	je	return_true
+	je	return_true	; Divisible by 400
 
 return_false:
 	mov	rax, 0
