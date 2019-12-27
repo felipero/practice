966722dd7a93ee7a3731bba9446297672bb9025f
Felipe Rodrigues <felipe@crafters.cc>
2019-12-27 10:35:45 -0300

    Adjusting swift leap solution to use structs computed properties. It required to change the specs too.

--- exercism/swift/leap/Sources/Leap/Leap.swift
+++ exercism/swift/leap/Sources/Leap/Leap.swift
@@ -1,7 +1,7 @@
 struct Year {
   let calendarYear: Int
 
-  func isLeapYear() -> Bool {
-    return calendarYear.isMultiple(of: 4) && (!calendarYear.isMultiple(of: 100) || calendarYear.isMultiple(of: 400))
+  var isLeapYear: Bool {
+    calendarYear.isMultiple(of: 4) && (!calendarYear.isMultiple(of: 100) || calendarYear.isMultiple(of: 400))
   }
 }
