b024e482d2bb66d226573650131b5d66fd27524b
Felipe Rodrigues <felipe@crafters.cc>
2019-12-30 02:09:38 -0300

    Improvement on Rust solution of reverse string using built in functions.

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,8 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut reverse = String::from("");
-
-    for c in input.chars() {
-        reverse.insert(0, c);
-    }
-    return reverse;
+    input.chars().rev().collect()
 }
