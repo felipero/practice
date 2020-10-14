9c4b3cec2a883b65274630b5829fa1ff7bfeb6a1
Felipe Rodrigues <felipe@crafters.cc>
2020-10-14 16:32:10 -0300

    new solutions for rust raindrops of exercism

--- exercism/rust/raindrops/src/lib.rs
+++ exercism/rust/raindrops/src/lib.rs
@@ -1,4 +1,33 @@
 pub fn raindrops(n: u32) -> String {
+    let res: String = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+        .iter()
+        .filter(|(e, _)| n % e == 0)
+        .map(|&(_, s)| s)
+        .collect();
+    match res.is_empty() {
+        true => n.to_string(),
+        false => res,
+    }
+}
+
+pub fn raindrops2(n: u32) -> String {
+    let mut res = String::new();
+
+    let parameters = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
+
+    for p in &parameters {
+        if n % p.0 == 0 {
+            res.push_str(p.1)
+        }
+    }
+
+    if res.is_empty() {
+        res = n.to_string()
+    }
+    res
+}
+
+pub fn raindrops1(n: u32) -> String {
     let mut result = String::from("");
     if n % 3 == 0 {
         result = result + "Pling";
